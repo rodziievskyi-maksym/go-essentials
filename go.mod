04b681a07f6e52d0ab43dd92a0d89d78a12f4f30
rodziievskyi-maksym <rodziyevskydev@gmail.com>
2025-01-07 00:01:44 +0200

    add auth service interface implementation examples

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rodziievskyi-maksym/go-essentials
 
 go 1.23.4
+
+require golang.org/x/crypto v0.32.0
---
+++ v2_2025/basics/interfaces.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService interface {
+	Login(username, password string) (string, error)
+	Signup(username, password string) (string, error)
+}
+
+//diff version of auth with simple and jwt
+
+type SimpleAuthService struct{}
+
+// methods with pointer receivers
+
+func (s *SimpleAuthService) Login(username, password string) (string, error) {
+	buffer := bytes.Buffer{}
+
+	buffer.WriteString(password)
+	buffer.WriteString(username)
+
+	return buffer.String(), nil
+}
+
+func (s *SimpleAuthService) Signup(username, password string) (string, error) {
+	buffer := bytes.Buffer{}
+
+	buffer.WriteString(username)
+	buffer.WriteString(password)
+
+	return buffer.String(), nil
+}
+
+func NewSimpleAuthService() AuthService {
+	return &SimpleAuthService{}
+}
+
+type JWTAuthService struct{}
+
+func (s *JWTAuthService) Login(username, password string) (string, error) {
+	return "", nil
+}
+func (s *JWTAuthService) Signup(username, password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	//generate JWT token
+
+	return string(hashedPassword), nil
+}
+
+func NewJWTAuthService() AuthService {
+	return &JWTAuthService{}
+}
+
+//user controller receives request
+
+type UserController struct {
+	authService AuthService
+}
+
+func (c *UserController) SignUpHandler(name, pass string) (string, error) {
+	//validate data
+	if name == "" && pass == "" {
+		return "", errors.New("invalid user data")
+	}
+
+	result, err := c.authService.Signup(name, pass)
+	if err != nil {
+		return "", errors.New("something went wrong")
+	}
+
+	return result, nil
+}
+
+func NewUserController(authService AuthService) *UserController {
+	return &UserController{authService: authService}
+}
+
+func main() {
+	//received data somewhere
+	userName := "max"
+	password := "test123"
+
+	authService := NewSimpleAuthService()
+
+	//eventually we might need to change auth service, but we already have realisation below and it might be huge.
+	//exactly for this reason I used AuthService Interface. I only have to make new JWT Auth Service and proceed it further.
+
+	//The day has come and new AuthService occurs.
+	//authService = NewJWTAuthService()
+
+	userController := NewUserController(authService)
+
+	result, err := userController.SignUpHandler(userName, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
+}
