d3e4a5047b8dd8529345d0746780d700382c0607
rodziievskyi-maksym <rodziyevskydev@gmail.com>
2025-01-19 10:40:17 +0200

    add channel example usage for comments

--- v2_2025/basics/concurrency/channels.go
+++ v2_2025/basics/concurrency/channels.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /*
 	How the channel struct look
@@ -31,7 +33,86 @@ import "fmt"
 func main() {
 	//playground with channels
 
-	ch := make(chan int, 4)
+	/*
+		this line of code allocates memory in the heap and save pointer (address) to hchan struct
 
-	fmt.Printf("channel %v", ch)
+		qcount = 0 at the moment
+		dataqsiz = 4
+
+		buf has unsafe.Pointer to buffer
+
+		recvX = 0
+		sendX = 0
+	*/
+
+	//1. Cone head. computer module. tube body. engine. 4 piece of hi-tech rocket
+	rocketLegoCh := make(chan string, 4)
+
+	/*
+		First of all we lock mutex to safely work a round with channel, and be afraid other post office send at the same time
+
+		Here I bump sendX value to 1, cuz sendX it is an index that indicates to buf's circular queue.
+		tell us
+
+		When to put data in the channel we COPY it, to further modification, original stays untouched unless we send a pointer
+
+		sendX = 1
+	*/
+	coneHeadPartOriginal := "Cone Head"
+	go NasaFactoryApproval(rocketLegoCh, coneHeadPartOriginal)
+
+	coneHeadPartApproved := <-rocketLegoCh
+
+	fmt.Println(coneHeadPartOriginal)
+	fmt.Println(coneHeadPartApproved)
+
+	/*
+		This example shows us how Circular Queue actually works, we fulled the buffer and sendX has no another option then
+		jump back to the beginning. This means channel is full, and it leads to another logic
+
+		sendX = 0
+
+		It will increase the index and write copy value to queue if buffer's cell is empty otherwise we move to another logic
+	*/
+	go NasaFactoryApproval(rocketLegoCh, coneHeadPartOriginal, "Computer Module", "Tube Body", "Engine")
+
+	/*
+		TOPIC NAME: buffer overflow
+
+		at this point we only send data to channel, and it is full. SpaceX comes late we're already building the rocket
+		this piece may go for the next rocket.
+
+		This goroutine is on hold (pause) by Go's scheduler
+	*/
+	go SpaceXFactoryApproval(rocketLegoCh, coneHeadPartOriginal)
+
+	//var rocketAssembler strings.Builder
+
+	fmt.Printf("channel %v", rocketLegoCh)
+}
+
+func NasaFactoryApproval(rocketLegoCh chan string, parts ...string) {
+	approvedText := " approved by NASA"
+
+	if len(parts) < 2 {
+		rocketLegoCh <- parts[0] + approvedText
+		return
+	}
+
+	for _, part := range parts {
+		rocketLegoCh <- part + approvedText
+	}
+}
+
+func SpaceXFactoryApproval(rocketLegoCh chan string, parts ...string) {
+	approvedText := " approved by SpaceX"
+
+	if len(parts) < 2 {
+		rocketLegoCh <- parts[0] + approvedText
+		return
+	}
+
+	for _, part := range parts {
+		rocketLegoCh <- part + approvedText
+	}
 }
